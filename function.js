2c5f40f948a5a27fd789d1d61c9e3c10610198a2
_KrX_ <131233706+Kritazya@users.noreply.github.com>
2024-03-29 20:09:03 -0500

    Create function.js

---
+++ function.js
@@ -0,0 +1,48 @@
+// Function => Declaring function => Calling function
+
+function krix()
+{
+    console.log("This is a funciton")
+
+}
+krix()
+
+//Parameters:
+function oop(a,b)
+{
+    console.log(a+b)
+}
+
+//arguments:
+oop(22,10)
+oop(092,2929)
+
+//Function return
+function efgh()
+{
+    return "This is my Function"
+}
+console.log(efgh())
+
+function abcd(a,b)
+{
+    return a+b
+}
+console.log(abcd(5,5))
+
+function mnop(a,b)
+{
+    console.log(a+b) 
+}
+mnop(5,5)              //can't used for further operations
+
+//Function Expressions es6
+var kk =function()
+{
+    return " Function Expressions"
+}
+console.log(kk())
+
+//arrow function
+var rr=(a) => a+a;
+console.log(rr(10))
